7cf93139558d1eeba8b1960c3dab194d9414b18a
Adam <adamomarali37@gmail.com>
2022-04-14 09:31:41 -0400

    Link to spreadsheet

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,6 +8,9 @@ function Home(props){
             <div className='padding-top'>
                 <Button variant="contained" onClick={() => props.setPage("editor")}>Start Match</Button>
             </div>
+            <div className='padding-top'>
+                <Button variant="contained"><a href="https://docs.google.com/spreadsheets/d/1mA4yFXa9aS_3zlellfbV3gmDuxMTdsApWvuJy311-2I/edit#gid=0" target="_blank">Spreadsheet</a></Button>
+            </div>
         </div>
     )
 }
