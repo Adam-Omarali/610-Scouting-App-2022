253c2df3ab54babc1c90fbaf1b500890b9e8da31
Adam <adamomarali37@gmail.com>
2022-04-15 10:34:30 -0400

    Remove data room button

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -28,9 +28,9 @@ function Home(props){
             <div className='padding-top'>
                 <Button variant="contained" onClick={() => changePage("editor")}>Start Match</Button>
             </div>
-            <div className='padding-top'>
+            {/* <div className='padding-top'>
                 <Button variant="contained" onClick={() => changePage("data")}>Data Room</Button>
-            </div>
+            </div> */}
             <div className='padding-top'>
                 <Button variant="contained"><a href="https://docs.google.com/spreadsheets/d/1CL8XwDPuNzGKTbXyt3ekynBhL7UYL8HeRJWB9_3mBmE/edit#gid=1441878395" target="_blank">Spreadsheet</a></Button>
             </div>
